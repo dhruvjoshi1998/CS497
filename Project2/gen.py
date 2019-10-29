d9f7ebf5a3e25f820396873bcd3ab3ab340991d5
Dhruv Joshi <dhruvjoshi1998@gmail.com>
2019-10-29 16:48:27 -0400

    structured

---
+++ Project2/gen.py
@@ -0,0 +1,11 @@
+"""
+Authors: Dhruv Joshi & Ethan Pullen
+Date: 29 October 2019
+File: gen.py
+Purpose: Generate words from the saved models
+"""
+
+
+import numpy as np
+import tensorflow as tf
+import keras
---
+++ Project2/train.py
@@ -0,0 +1,10 @@
+"""
+Authors: Dhruv Joshi & Ethan Pullen
+Date: 29 October 2019
+File: train.py
+Purpose: Train and save models to Models folder
+"""
+
+import numpy as np
+import tensorflow as tf
+import keras
