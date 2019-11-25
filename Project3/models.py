c12f10f6b3a1add8237be7c55a74a3c1134e42a9
Dhruv Joshi <dhruvjoshi1998@gmail.com>
2019-11-24 21:14:27 -0500

    model compiling

--- Project3/models.py
+++ Project3/models.py
@@ -10,21 +10,21 @@ NUM_PIPELINES = 2
 
 def make_cnn():
 	model = Sequential()
-	model.add(Conv2D(128, kernel_size = (15, 15), strides = 2, activation='relu', input_shape = (1024, 768, 3)))
-	model.add(Conv2D(256, kernel_size = (7, 7), strides = 2, activation='relu'))
+	model.add(Conv2D(64, kernel_size = (15, 15), strides = 2, activation='relu', input_shape = (1024, 768, 3)))
+	model.add(Conv2D(128, kernel_size = (7, 7), strides = 2, activation='relu'))
 	for _ in range(NUM_PIPELINES):
-		model.add(Conv2D(1024, kernel_size = (3, 3), activation='relu'))
-		model.add(Conv2D(1024, kernel_size = (5, 5), activation='relu'))
+		model.add(Conv2D(256, kernel_size = (3, 3), activation='relu'))
+		model.add(Conv2D(256, kernel_size = (5, 5), activation='relu'))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(rate=0.125))
 	model.add(Flatten())
-	model.add(Dense(1024, activation = 'relu'))
+	model.add(Dense(512, activation = 'relu'))
 	return model
 
 def make_mlp():
 	model = Sequential()
-	model.add(Dense(10, activation = 'sigmoid', input_shape = (2,)))
-	model.add(Dense(10, activation = 'sigmoid'))
+	model.add(Dense(10, activation = 'relu', input_shape = (2,)))
+	model.add(Dense(10, activation = 'relu'))
 	return model
 
 def mixed_model():
@@ -33,10 +33,12 @@ def mixed_model():
 
 	cout = concatenate([mlp.output, cnn.output])
 
-	x = Dense(1024, activation = 'sigmoid')(cout)
-	x = Dense(1024, activation = 'sigmoid')(x)
-	x = Dense(512, activation = 'sigmoid')(x)
-	x = Dense(256, activation = 'sigmoid')(x)
+	x = Dense(256, activation = 'relu')(cout)
+	x = Dense(256, activation = 'relu')(x)
+	x = Dense(128, activation = 'relu')(x)
+	x = Dense(64, activation = 'relu')(x)
 	x = Dense(1, activation = 'sigmoid')(x)
 
 	model = Model([mlp.input,cnn.input], x)
+
+	return model
